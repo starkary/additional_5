323934be7b5970fe16936eb13a1fb6f31b72746b
Kary <starkary70@gmail.com>
2018-09-30 23:46:27 +0300

    tarting

--- src/index.js
+++ src/index.js
@@ -1,3 +1,24 @@
 module.exports = function check(str, bracketsConfig) {
-  // your solution
+ const stack = [];
+ for(let i = 0; i < str.length; i++){
+  configLoop: for (let j = 0; j < bracketsConfig.length; j++){
+   for( let k = 0; k < bracketsConfig[j].length; k++){
+    if(str[i] === bracketsConfig[j][k]){
+     if(k === 0 && stack[stack.length - 1] !== str[i]){
+      stack.push(bracketsConfig[j][1]);
+      break configLoop;
+     }else if(stack.pop() === str[i]){
+      break configLoop;
+     } else {
+      return false;
+     }
+     
+    }
+   }
+   
+  }
+
+ 
+ }
+ return !stack.length;
 }
